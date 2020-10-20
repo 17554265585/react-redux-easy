96f673826cc18707fea2a3320a465ae1602bcc86
zhangchao <zhangchao@work.com>
2020-10-20 09:13:02 +0800

    init

---
+++ example/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClassComponent from './modules/ClassComponent';
+// import HooksComponents from './modules/HooksComponents';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ClassComponent id={8} />
+    {/* <HooksComponents /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
